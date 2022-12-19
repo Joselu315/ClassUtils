a1db7b59334f8376866b95068b71914b45c81ae0
Joseluu <77939750+Joselu315@users.noreply.github.com>
2022-12-19 20:56:42 +0100

    refactor -> change message when dependency load

--- src/main/java/com/github/joseluu/Main.java
+++ src/main/java/com/github/joseluu/Main.java
@@ -2,6 +2,6 @@ package com.github.joseluu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Utils dependency load with exits code by Joselu315");
     }
 }
