e93d396ecfe9243f7bfdab77e1aec7e1498b2e50
Joseluu <77939750+Joselu315@users.noreply.github.com>
2023-03-19 12:10:11 +0100

    feat -> add a guice impl to main class

--- src/main/java/com/github/joseluu/Main.java
+++ src/main/java/com/github/joseluu/Main.java
@@ -1,7 +1,19 @@
 package com.github.joseluu;
 
+import com.github.joseluu.guice.GuiceModule;
+import com.github.joseluu.guice.service.GuiceService;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Utils dependency load with exits code by Joselu315");
+
+        //Here you can a example code about how to work guice in main class
+        Injector injector = Guice.createInjector(new GuiceModule());
+        GuiceService guiceService = injector.getInstance(GuiceService.class);
+        guiceService.start();
+        guiceService.stop();
+        //Here you can a example code about how to work guice in main class
     }
 }
